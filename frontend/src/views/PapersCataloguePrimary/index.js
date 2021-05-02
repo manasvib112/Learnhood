20636f554849f4dc228646124d601b14e3860f56
Manasvi Soni <manasvib112@gmail.com>
2021-05-02 19:55:14 +0530

    successfully created HOC

--- frontend/src/views/PapersCataloguePrimary/index.js
+++ frontend/src/views/PapersCataloguePrimary/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
+import Catalogue from '../../components/Catalogue'
 import background from '../../assets/images/background1.png'
 import './style.css'
 import tp from '../../assets/images/termpaper.png'
@@ -10,26 +11,38 @@ export default function PapersCataloguePrimary(props) {
   const { year } = useParams()
   console.log(year)
   return (
-    <div className='papers-primary-cat-container'>
-      <Header background={background} />
-      <Sidebar filter={{ subject: [{ a: 12, b: 12 }] }} />
-      <div className='papers-primary-cat-main'>
-        <h1> Previous years paper for {year} year students</h1>
-        <div className='term-paper-row'>
-          <div className='term-paper'>
-            <h3>T1</h3>
-            <img src={tp} alt='term paper' />
-          </div>
-          <div className='term-paper'>
-            <h3>T2</h3>
-            <img src={tp} alt='term paper' />
-          </div>
-          <div className='term-paper'>
-            <h3>T3</h3>
-            <img src={tp} alt='term paper' />
-          </div>
+    <Catalogue
+      header={() => {
+        return <Header background={background} />
+      }}
+      sidebar={() => {
+        return <Sidebar filter={{ subject: [{ a: 12, b: 12 }] }} />
+      }}
+      content={() => {
+        return <Content year={year} />
+      }}
+    />
+  )
+}
+
+const Content = ({ year }) => {
+  return (
+    <>
+      <h1> Previous years paper for {year} year students</h1>
+      <div className='term-paper-row'>
+        <div className='term-paper'>
+          <h3>T1</h3>
+          <img src={tp} alt='term paper' />
+        </div>
+        <div className='term-paper'>
+          <h3>T2</h3>
+          <img src={tp} alt='term paper' />
+        </div>
+        <div className='term-paper'>
+          <h3>T3</h3>
+          <img src={tp} alt='term paper' />
         </div>
       </div>
-    </div>
+    </>
   )
 }
